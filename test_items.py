02edc0208b13d0746fb0c506b0b85748fe30872a
Tilen52 <tilen6452@gmail.com>
2019-11-12 06:04:34 +0300

    edited the time.sleep to 30 secs

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_does_add_to_basket_button_exist(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     button = len(browser.find_elements_by_css_selector('.btn-add-to-basket'))
     assert button > 0, '!!!НЕ НАШЕЛ КНОПКУ!!!'
