9bd720e23540c9902ef53ea91cefc20afff7f3e4
Tilen52 <tilen6452@gmail.com>
2019-11-12 06:01:52 +0300

    added assert

--- test_items.py
+++ test_items.py
@@ -2,6 +2,8 @@ import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_does_basket_button_exist(browser):
+def test_does_add_to_basket_button_exist(browser):
     browser.get(link)
-    browser.find_element_by_css_selector('.btn-add-to-basket')
+    time.sleep(5)
+    button = len(browser.find_elements_by_css_selector('.btn-add-to-basket'))
+    assert button > 0, '!!!НЕ НАШЕЛ КНОПКУ!!!'
